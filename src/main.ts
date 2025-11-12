7773da2f2fc2218ff2eec4d25c2dffe74fed5cc2
federicomilanilavoro <federicomilanilavoro@gmail.com>
2025-11-12 12:36:06 +0100

    Fix CORS

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,8 @@ async function bootstrap() {
 //AppModule è il “modulo radice” che contiene tutti gli altri (Customer, Material, Order…).
 
   app.enableCors({
-    origin: true,
+    origin: ['https://gestionale.marmimilani.it', 'http://localhost:5173'],
+    credentials: true,
     methods: ['GET','POST','PUT','DELETE','OPTIONS'],
     allowedHeaders: ['Content-Type','Authorization'],
   })
